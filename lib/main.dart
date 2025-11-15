8c2efd1ebcc34cd2ccb3f2a67539bf2f035724c2
soroushsalehzadeh <soroushsalehzadeh3@gmail.com>
2025-11-15 20:40:22 +0330

    توضیح کوتاه درباره تغییرات

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,10 @@ void main() {
   runApp(const MyApp());
 }
 
+//dfvbn
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  //kl
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -105,10 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
